b1a6da88579e4012187bcdab4c3967ab6312d76f
guilherme <guilherme@cargon.com.br>
2021-07-05 13:44:41 -0300

    Upload de foto de perfil

--- src/routes/users/route.ts
+++ src/routes/users/route.ts
@@ -1,7 +1,13 @@
-import { Router } from "express";
+import { request, Router } from "express";
+import multer from "multer";
+
 import { CreateUserService } from "../../services/CreateUserService";
+import uploadConfig from "../../config/upload";
+import { authMiddleware } from "../../middlewares/authMiddleware";
 
 const userRouter = Router();
+
+const upload = multer(uploadConfig);
 
 userRouter.get("/", async (request, response) => {
   response.send();
@@ -13,12 +19,21 @@ userRouter.post("/", async (request, response) => {
 
     const createUserService = new CreateUserService();
 
-    const user = await createUserService.execute({ name, email, password });    
+    const user = await createUserService.execute({ name, email, password });
 
     return response.status(201).json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
+
+userRouter.patch(
+  "/avatar",
+  authMiddleware,
+  upload.single("avatar"),
+  (request, response) => {
+    return response.json(request.file);
+  }
+);
 
 export { userRouter };
